65ec6e8d93c534873d4e97ca636683d29442ba64
nailykhry <nailykhairiya@gmail.com>
2024-12-22 22:41:31 +0700

    add: margin top at status load

--- app/components/StatusLoad.js
+++ app/components/StatusLoad.js
@@ -2,12 +2,12 @@ export default function StatusLoad({ loading, hasMore }) {
   return (
     <>
       {loading && (
-        <div className="mb-10 text-center">
+        <div className="mt-10 mb-10 text-center">
           <span className="text-gray-600">Loading more contents...</span>
         </div>
       )}
       {!hasMore && (
-        <div className="mb-10 text-center">
+        <div className="mt-10 mb-10 text-center">
           <span className="text-gray-600">No more content to load.</span>
         </div>
       )}
